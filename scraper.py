7d6dfcff746e5370b87dd44c493f1a9f24bd9075
Justin <justin@MacBook-Pro-von-Admin.local>
2022-11-09 15:29:13 +0100

    update

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-import os
+import os, csv
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -7,7 +7,8 @@ from selenium.webdriver.common.by import By
 
 url = "https://www.ardmediathek.de/"
 chromedriver_path="/Users/justin/Downloads/chromedriver"
-screenshots_dir="./screenshots"
+screenshots_path="./screenshots"
+csv_file_path="./ard.csv"
 
 def set_up():
     print("\nSetting up the browser...")
@@ -30,23 +31,23 @@ def set_up():
 def take_screenshots():
     print("\nDeleting previous screenshots...")
 
-    for file in os.scandir(dir):
+    for file in os.scandir(screenshots_path):
         os.remove(file.path)
 
-    print("Taking screenshots...")
-    
+    print("\nTaking screenshots...")
+
     sleep(5)
     
     page_content = browser.find_element(By.CSS_SELECTOR, "body > div > div")
-    page_content.screenshot("./screenshots/full-page.png")
+    page_content.screenshot("{}/full-page.png".format(screenshots_path))
     print("Saved screenshot 'full-page.png'")
 
     stage_content = browser.find_elements(By.CSS_SELECTOR, "div > div > a > div > img")
     next_button = browser.find_element(By.CSS_SELECTOR, ".swiper-button-next")
 
     for index, stage_img in enumerate(stage_content):
-        stage_img.screenshot("./screenshots/{}-stage.png".format(index))
-        print("Saved screenshot '{}-stage.png'".format(index))
+        stage_img.screenshot("{}/{}-stage.png".format(screenshots_path, index + 1))
+        print("Saved screenshot '{}-stage.png'".format(index + 1))
 
         next_button.click()
         sleep(1)
@@ -54,14 +55,19 @@ def take_screenshots():
 def get_video_titles():
     print("\nFetching video titles...")
 
-    output = { "Stage": [] }
-    rubric_titles = browser.find_elements(By.CSS_SELECTOR, "section h2")
-    rubrics = browser.find_elements(By.CSS_SELECTOR, ".swiper-wrapper")
+    global rubric_titles
+    rubric_titles = []
 
-    for rubric_title in rubric_titles:
-        output[rubric_title.text] = []
+    global video_titles
+    video_titles = []
 
-    for rubric in rubrics:
+    rubric_title_elements = browser.find_elements(By.CSS_SELECTOR, "section h2")
+    rubric_elements = browser.find_elements(By.CSS_SELECTOR, ".swiper-wrapper")
+
+    for rubric_title in rubric_title_elements:
+        rubric_titles.append(rubric_title.text)
+
+    for rubric in rubric_elements:
         results = []
 
         try:
@@ -74,13 +80,23 @@ def get_video_titles():
         except:
             pass
         
-        print(results)
+        video_titles.append(results)
 
-    print(output, len(output), len(rubrics))
+def write_to_csv_file():
+    print("\nWriting to CSV file...")
+
+    with open(csv_file_path, "w+", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+
+        writer.writerow(rubric_titles)
+        writer.writerows(video_titles)
+
+    print(rubric_titles, "\n", video_titles)
 
 if __name__ == "__main__":
     set_up()
     take_screenshots()
     get_video_titles()
+    write_to_csv_file()
 
     browser.quit()
